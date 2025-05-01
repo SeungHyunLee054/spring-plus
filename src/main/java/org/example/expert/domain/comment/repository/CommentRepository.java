78f8e1a8ffd12a8a1bedd4359a48d90f70b5ec09
SeungHyunLee054 <yellow13lsh@naver.com>
2025-05-01 09:12:05 +0900

    Lv 2-2 : N+1

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 	@EntityGraph(attributePaths = {"user"})
-	List<Comment> findByTodo_Id(Long todoId);
+	List<Comment> findWithUserByTodo_Id(Long todoId);
 }
