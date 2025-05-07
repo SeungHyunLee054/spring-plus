39bf365f1d2842b27a8ede40472435d919f7cb8a
SeungHyunLee054 <yellow13lsh@naver.com>
2025-05-07 11:32:33 +0900

    Lv 4 : Kotlin 적용하기

--- src/main/java/org/example/expert/domain/user/controller/UserAdminController.kt
+++ src/main/java/org/example/expert/domain/user/controller/UserAdminController.kt
@@ -1,6 +1,5 @@
 package org.example.expert.domain.user.controller
 
-import lombok.RequiredArgsConstructor
 import org.example.expert.domain.user.dto.request.UserRoleChangeRequest
 import org.example.expert.domain.user.service.UserAdminService
 import org.springframework.web.bind.annotation.PatchMapping
@@ -9,12 +8,10 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequiredArgsConstructor
-class UserAdminController {
-    private val userAdminService: UserAdminService? = null
+class UserAdminController(private val userAdminService: UserAdminService) {
 
     @PatchMapping("/admin/users/{userId}")
     fun changeUserRole(@PathVariable userId: Long, @RequestBody userRoleChangeRequest: UserRoleChangeRequest) {
-        userAdminService!!.changeUserRole(userId, userRoleChangeRequest)
+        userAdminService.changeUserRole(userId, userRoleChangeRequest)
     }
 }
